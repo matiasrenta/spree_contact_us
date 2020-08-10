287b92ee1c20202ec6552fd272c87256e51ea01a
Matias Renta <matiasrenta@gmail.com>
2020-08-10 19:07:22 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.add_routes do
   resources :contacts, controller: 'contact_us/contacts', only: %i[new create]
   get 'contact-us' => 'contact_us/contacts#new', as: :contacto
-  post 'contact-us' => 'contact_us/contacts#create', as: :contacto
+  post 'contact-us' => 'contact_us/contacts#create'
 end
